daa391b911bcc86de6fd273baa00e5e7b97fca7e
Ayman Anaam <31734218+ihkd2000@users.noreply.github.com>
2024-08-26 14:58:35 +0000

    update connetivity

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ if(builder.Environment.IsDevelopment())
     // builder.Services.AddDbContext<MyDatabaseContext>(options =>
     //     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     // builder.Services.AddDistributedMemoryCache();
-    builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseMySql(builder.Configuration.GetConnectionString("MyDbConnection"), 
-        new MySqlServerVersion(new Version(8, 0, 21)))); // Replace with your MySQL version
+    // builder.Services.AddDbContext<MyDatabaseContext>(options =>
+    //     options.UseMySql(builder.Configuration.GetConnectionString("MyDbConnection"), 
+    //     new MySqlServerVersion(new Version(8, 0, 21)))); // Replace with your MySQL version
+        builder.Services.AddDbContext<MyDatabaseContext>(options =>
+        options.UseMySQL(builder.Configuration.GetConnectionString("MyDbConnection")));
+
     builder.Services.AddDistributedMemoryCache();
 
 }
